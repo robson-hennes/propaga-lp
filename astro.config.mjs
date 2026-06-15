62e736fe064c3b7a0b2acff5fad946c630c6582d
Robson Hennes <devrobsonhennes@users.noreply.github.com>
2026-06-15 19:03:07 +0000

    fix: ajustar base path do GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://robson-hennes.github.io',
-  base: '/propaga-lp',
+  base: '/propaga-lp/',
   output: 'static',
 });
